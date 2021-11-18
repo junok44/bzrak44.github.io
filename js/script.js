dab6172ab2fc2c607900696cfe95fea40b261e18
junok <89637844+junok44@users.noreply.github.com>
2021-11-18 14:29:30 +0900

    change source

--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
 $(document).ready(function () {
 
-    // // 위로가기 기능
-    // $('.gotop').click(function () {
-    //     $('html, body').animate({
-    //         scrollTop: 0
-    //     });
-    // });
-
     // 주메뉴 fadeout
     let header = $('.header');
     $(window).scroll(function () {
@@ -19,24 +12,19 @@ $(document).ready(function () {
         }
     });
 
+
     // 주메뉴 이동
     var moveEl = $('.move');
     $.each(moveEl, function () {
         $(this).click(function (e) {
-            // href 를 일단 막아서 위치이동 못하게함.
             e.preventDefault();
-            // 가야하는 곳의 위치를 파악한다.
             var tg = $(this).attr('href');
             var num;
-            // 혹시 #아이디 가 없다면
             if (tg == '#') {
                 num = 0;
             } else {
-                // 어느 만큼 이동해야 하는 지를 숫자로 파악한다.
                 num = $(tg).offset().top;
             }
-
-            // 움직이자
             $('html, body').stop().animate({
                 scrollTop: num
             }, 800);
@@ -47,27 +35,17 @@ $(document).ready(function () {
     $('.visual-tit').waypoint(function (dir) {
         if (dir == "down") {
             $('.visual-tit').addClass('visual-tit-active');
-        } else {
-            // $('.visual-story').removeClass('visual-story-active');
-        }
+        } else {}
     }, {
         offset: '100%'
     });
 
 
-    $('.visual-img').waypoint(function (dir) {
-        if (dir == "down") {
-            $('.visual-img').addClass('.visual-img-active');
-        } else {
-            // $('.visual-st ory').removeClass('visual-story-active');
-        }
-    }, {
-        offset: '100%'
-    });
     let sw_visual = new Swiper('.sw-visual', {
         effect: "fade",
+        loop: true,
         autoplay: {
-            delay: 1500,
+            delay: 2000,
         },
     });
 
@@ -75,17 +53,33 @@ $(document).ready(function () {
     $('.about-pic').waypoint(function (dir) {
         if (dir == "down") {
             $('.about-pic').addClass('about-pic-active');
-        } else {
-            // $('.visual-story').removeClass('visual-story-active');
-        }
+        } else {}
     }, {
         offset: '75%'
+    });
+    // 비주얼이동
+    var move_visual = $('.move-visual');
+    $.each(move_visual, function () {
+        $(this).click(function (e) {
+            e.preventDefault();
+            var tg = $(this).attr('href');
+            var num;
+            if (tg == '#') {
+                num = 0;
+            } else {
+                num = $(tg).offset().top;
+            }
+            $('html, body').stop().animate({
+                scrollTop: num
+            }, 800);
+        });
     });
 
 
     // 리디자인 슬라이드
     let sw_rede = new Swiper(".sw-rede", {
         effect: "fade",
+        // loop: true,
     });
 
     let rede_bot_a = $('.rede-bot a');
@@ -120,64 +114,6 @@ $(document).ready(function () {
         });
     });
 
-    // // 말줄임 적용 (...)
-    // $(".publ-desc").dotdotdot({
-    //     wrapper: 'div',
-    //     /*  콘텐트를 감쌀 요소. */
-    //     ellipsis: '... ',
-    //     /*  말줄임표를 뭘로 할지 */
-    //     wrap: 'word',
-    //     /*  자를 단위. 다음 옵션 중 하나 선택: 'word'/'letter'/'children' */
-    //     after: null,
-    //     /*  자르고 나서도 유지시킬 요소를 jQuery 선택자로 적는다. */
-    //     watch: false,
-    //     /*  윈도우가 리사이즈될 때 업데이트할 건지: true/'window' */
-    //     height: null,
-    //     /*  선택. max-height를 지정한다. 만약 null이면 알아서 잰다. */
-    //     tolerance: 0 /*  글이 넘치면 이만큼쯤 height를 늘린다 */
-    // });
-
-    // $(".rede-box-desc p").dotdotdot({
-    //     wrapper: 'div',
-    //     /*  콘텐트를 감쌀 요소. */
-    //     ellipsis: '... ',
-    //     /*  말줄임표를 뭘로 할지 */
-    //     wrap: 'word',
-    //     /*  자를 단위. 다음 옵션 중 하나 선택: 'word'/'letter'/'children' */
-    //     after: null,
-    //     /*  자르고 나서도 유지시킬 요소를 jQuery 선택자로 적는다. */
-    //     watch: false,
-    //     /*  윈도우가 리사이즈될 때 업데이트할 건지: true/'window' */
-    //     height: null,
-    //     /*  선택. max-height를 지정한다. 만약 null이면 알아서 잰다. */
-    //     tolerance: 0 /*  글이 넘치면 이만큼쯤 height를 늘린다 */
-    // });
-
-    // // 부드럽게 위치 이동하는 코드
-    // var moveEl = $('.move');
-    // // 클릭시 해당위치로 부드럽게 이동한다.
-    // $.each(moveEl, function () {
-
-    //     $(this).click(function (e) {
-    //         // href 를 일단 막아서 위치이동 못하게함.
-    //         e.preventDefault();
-    //         // 가야하는 곳의 위치를 파악한다.
-    //         var tg = $(this).attr('href');
-    //         var num;
-    //         // 혹시 #아이디 가 없다면
-    //         if (tg == '#') {
-    //             num = 0;
-    //         } else {
-    //             // 어느 만큼 이동해야 하는 지를 숫자로 파악한다.
-    //             num = $(tg).offset().top;
-    //         }
-
-    //         // 움직이자
-    //         $('html, body').stop().animate({
-    //             scrollTop: num
-    //         }, 500);
-    //     });
-    // });
 
     // 성격 프로그레스바
     var list1 = new ProgressBar.Line(char_list1, {
@@ -384,33 +320,9 @@ $(document).ready(function () {
         }
 
     });
-    // 브라우저 고정
 
     // etc
     $('.gallery-item a').colorbox();
-
-    // $('.count').counterUp({
-    //     delay: 10,
-    //     time: 1000
-    // });
-
-    // // 스크롤시 애니메이션 
-    // $('.life-img').waypoint(function (dir) {
-    //     if (dir == "down") {
-    //         $('.life-img').addClass('life-img-active');
-    //     } else {
-    //         $('.life-img').removeClass('life-img-active');
-    //     }
-    // }, {
-    //     offset: '100%'
-    // });
-
-    // $(".etc-sample").niceScroll({
-    //     cursorwidth: "20px",
-    //     cursoropacitymax: 0.5,
-    //     boxzoom: true,
-    //     background: "#000"
-    // });
 
 });
 
